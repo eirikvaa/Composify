3bc081ffac1eb4bef27668ee4f543539a9638cdc
Eirik Vale Aase <e.v.aase@gmail.com>
2016-11-05 20:26:12 +0100

    Revert "Gives user the option to change the sections and/or project of a recording when it has been recorded."
    This reverts commit 78c30cb7ae7d39d5a45ebecb35a1dfe2ff91b1f8.

--- Piece/SectionsTableViewController.swift
+++ Piece/SectionsTableViewController.swift
@@ -45,7 +45,7 @@ class SectionsTableViewController: UITableViewController {
 
 	// MARK: Properties
 	var chosenProject: Project!
-	fileprivate var managedObjectContext = CoreDataStack.sharedInstance.managedContext
+	fileprivate var managedObjectContext = CoreDataStack.sharedInstance.persistentContainer.viewContext
 	fileprivate lazy var fetchedResultsController: NSFetchedResultsController<NSFetchRequestResult> = {
 		let fetchRequest: NSFetchRequest<NSFetchRequestResult> = Section.fetchRequest()
 		let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
@@ -109,7 +109,7 @@ class SectionsTableViewController: UITableViewController {
 				if let title = renameAlert.textFields?.first?.text {
 					let section = self.sections[indexPath.row]
 
-					PIEFileManager().rename(section, from: section.title, to: title, section: nil, project: nil)
+					PIEFileManager().rename(section, from: section.title, to: title)
 					section.title = title
 					CoreDataStack.sharedInstance.saveContext()
 				}
