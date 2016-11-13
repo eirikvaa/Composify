22bd350e4246cb3235eda210b1a501c11675c5d9
Eirik Vale Aase <e.v.aase@gmail.com>
2016-11-13 20:40:17 +0100

    Add UI tests for sections table.

---
+++ PieceUITests/SectionsTableTests.swift
@@ -0,0 +1,114 @@
+//
+//  SectionsTableTests.swift
+//  Piece
+//
+//  Created by Eirik Vale Aase on 13.11.2016.
+//  Copyright © 2016 Eirik Vale Aase. All rights reserved.
+//
+
+import XCTest
+import CoreData
+@testable import Piece
+
+class SectionsTableTests: XCTestCase {
+	
+	let app = XCUIApplication()
+	
+	override func setUp() {
+		super.setUp()
+		
+		// Put setup code here. This method is called before the invocation of each test method in the class.
+		
+		// In UI tests it is usually best to stop immediately when a failure occurs.
+		continueAfterFailure = false
+		// UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+		app.launchArguments = [
+			"UI_TEST_MODE_SECTIONS",
+			"-AppleLanguages", "(nb-NO)",
+			"-AppleLocale", "\"nb-NO\""
+		]
+		
+		app.launch()
+		
+		// In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+	}
+	
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testEmptyProjectSectionsList() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+		
+		app.tables.staticTexts["Something New"].tap()
+		XCTAssertTrue(app.tables.element.staticTexts.count == 0)
+    }
+	
+	func testAddSection() {
+		app.tables.staticTexts["Something New"].tap()
+		
+		let somethingNewNavigationBar = app.navigationBars["Something New"]
+		somethingNewNavigationBar.buttons["Rediger"].tap()
+		somethingNewNavigationBar.buttons["Legg til"].tap()
+		
+		let nySeksjonAlert = app.alerts["Ny seksjon"]
+		nySeksjonAlert.collectionViews.textFields["Tittel på seksjon"].typeText("Intro")
+		nySeksjonAlert.buttons["Lagre"].tap()
+		somethingNewNavigationBar.buttons["Ferdig"].tap()
+		
+		let table = app.tables.element
+		
+		XCTAssertTrue(table.staticTexts["Intro"].exists)
+		XCTAssertTrue(table.staticTexts.count == 1)
+	}
+	
+	func testRenameSection() {
+		let tablesQuery = app.tables
+		tablesQuery.staticTexts["Something New"].tap()
+		
+		let somethingNewNavigationBar = app.navigationBars["Something New"]
+		somethingNewNavigationBar.buttons["Rediger"].tap()
+		somethingNewNavigationBar.buttons["Legg til"].tap()
+		
+		let nySeksjonAlert = app.alerts["Ny seksjon"]
+		nySeksjonAlert.collectionViews.textFields["Tittel på seksjon"].typeText("Verse")
+		nySeksjonAlert.buttons["Lagre"].tap()
+		tablesQuery.buttons["Slett Verse"].tap()
+		tablesQuery.buttons["Gi nytt navn"].tap()
+		
+		let giNyttNavnAlert = app.alerts["Gi nytt navn"]
+		giNyttNavnAlert.collectionViews.textFields["Nytt navn til seksjon"].typeText("Outro")
+		giNyttNavnAlert.buttons["Lagre"].tap()
+		somethingNewNavigationBar.buttons["Ferdig"].tap()
+		
+		let table = app.tables.element
+		
+		XCTAssertTrue(table.staticTexts["Outro"].exists)
+		XCTAssertFalse(table.staticTexts["Verse"].exists)
+		XCTAssertTrue(table.staticTexts.count == 1)
+	}
+	
+	func testDeleteSection() {
+		let tablesQuery = app.tables
+		tablesQuery.staticTexts["Something New"].tap()
+		
+		let somethingNewNavigationBar = app.navigationBars["Something New"]
+		somethingNewNavigationBar.buttons["Rediger"].tap()
+		somethingNewNavigationBar.buttons["Legg til"].tap()
+		
+		let nySeksjonAlert = app.alerts["Ny seksjon"]
+		nySeksjonAlert.collectionViews.textFields["Tittel på seksjon"].typeText("Verse")
+		nySeksjonAlert.buttons["Lagre"].tap()
+		tablesQuery.buttons["Slett Verse"].tap()
+		tablesQuery.buttons["Slett"].tap()
+		somethingNewNavigationBar.buttons["Ferdig"].tap()
+		
+		let table = app.tables.element
+		
+		XCTAssertFalse(table.staticTexts["Verse"].exists)
+		XCTAssertTrue(table.staticTexts.count == 0)
+	}
+    
+}
