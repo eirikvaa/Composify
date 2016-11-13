a7c41bb8f2dda926b757480492b74141908c1837
Eirik Vale Aase <e.v.aase@gmail.com>
2016-11-13 21:41:24 +0100

    Extract logic for setting up test mode into own class. Remove empty delegate methods.

--- Piece/AppDelegate.swift
+++ Piece/AppDelegate.swift
@@ -13,76 +13,36 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
-	var testing = false
-	
-	private func reset() {
-		// Wipe all projects.
-		let fetchRequest = Project.fetchRequest()
-		var projects = [Project]()
-		
-		do {
-			projects = try CoreDataStack.sharedInstance.managedContext.fetch(fetchRequest) as! [Project]
-		} catch {
-			print(error.localizedDescription)
-		}
-		
-		for project in projects {
-			PIEFileManager().delete(project)
-		}
-		
-		let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
-		
-		do {
-			try CoreDataStack.sharedInstance.persistentContainer.persistentStoreCoordinator.execute(deleteRequest, with: CoreDataStack.sharedInstance.managedContext)
-		} catch {
-			print(error.localizedDescription)
-		}
-	}
+	var testInitializer: TestInitializer!
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
 		
-		let args = ProcessInfo.processInfo.arguments
-		if args.contains("UI_TEST_MODE") {
-			testing = true
-			reset()
-		}
+		let launchArguments = ProcessInfo.processInfo.arguments
+		testInitializer = TestInitializer(arguments: launchArguments)
+		testInitializer.setupTestingMode()
 		
 		return true
-	}
-
-	func applicationWillResignActive(_ application: UIApplication) {
-		// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-		// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 	}
 
 	func applicationDidEnterBackground(_ application: UIApplication) {
 		// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
 		// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-		if !testing {
+		if !testInitializer.active {
 			CoreDataStack.sharedInstance.saveContext()
 		}
-	}
-
-	func applicationWillEnterForeground(_ application: UIApplication) {
-		// Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-	}
-
-	func applicationDidBecomeActive(_ application: UIApplication) {
-		// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 	}
 
 	func applicationWillTerminate(_ application: UIApplication) {
 		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 		
-		if !testing {
+		if !testInitializer.active {
 			CoreDataStack.sharedInstance.saveContext()
 		} else {
-			reset()
+			testInitializer.reset()
 		}
 		
 	}
-
 
 }
 
