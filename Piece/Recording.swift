18593e40fe76e856eb63e34dc6b0379ee81786a8
Eirik Vale Aase <e.v.aase@gmail.com>
2016-11-26 16:16:06 +0100

    Conform to equatable protocol and create new initializer for inserting object directly into managed object context.

--- Piece/Recording.swift
+++ Piece/Recording.swift
@@ -10,5 +10,23 @@ import Foundation
 import CoreData
 
 class Recording: NSManagedObject {
-
+	convenience init?(with title: String, section: Section, project: Project, fileExtension: FileSystemExtensions, insertIntoManagedObjectContext context: NSManagedObjectContext!) {
+		let entity = NSEntityDescription.entity(forEntityName: "Recording", in: context)!
+		self.init(entity: entity, insertInto: context)
+		
+		self.title = title
+		self.section = section
+		self.project = project
+		self.fileExtension = fileExtension.rawValue
+		self.dateRecorded = Date()
+	}
+	
+	static func ==(lhs: Recording, rhs: Recording) -> Bool {
+		return
+			lhs.title == rhs.title &&
+			lhs.section == rhs.section &&
+			lhs.project == rhs.project &&
+			lhs.fileExtension == rhs.fileExtension &&
+			lhs.dateRecorded == rhs.dateRecorded
+	}
 }
