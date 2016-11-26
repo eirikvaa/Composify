ca4450623e636db60584e34365fc96b1f872e2e2
Eirik Vale Aase <e.v.aase@gmail.com>
2016-11-26 16:16:24 +0100

    Conform to equatable protocol.

--- Piece/Project.swift
+++ Piece/Project.swift
@@ -10,5 +10,10 @@ import Foundation
 import CoreData
 
 class Project: NSManagedObject {
-
+	static func ==(lhs: Project, rhs: Project) -> Bool {
+		return
+			lhs.title == rhs.title &&
+			lhs.sections == rhs.sections &&
+			lhs.recordings == rhs.recordings
+	}
 }
