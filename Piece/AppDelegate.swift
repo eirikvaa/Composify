ce3de2e20bea502ff5f2bbde1c964c332ce13807
Eirik Vale Aase <e.v.aase@gmail.com>
2017-07-02 21:54:07 +0200

    Remove unnecessary code

--- Piece/AppDelegate.swift
+++ Piece/AppDelegate.swift
@@ -12,52 +12,38 @@ import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-	var window: UIWindow?
-	private var testInitializer: TestInitializer!
+    var window: UIWindow?
 
-	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {		
-		// `persistentContainer` is marked 'lazy', so initialize to ready it for use.
-		_ = CoreDataStack.sharedInstance.viewContext
-		
-		let launchArguments = ProcessInfo.processInfo.arguments
-		testInitializer = TestInitializer(arguments: launchArguments)
-		testInitializer.setupTestingMode()
-		
-		let navigationBarAppearance = UINavigationBar.appearance()
-		navigationBarAppearance.barTintColor = UIColor(red: 231.0/255.0, green: 76.0/255.0, blue: 60.0/255.0, alpha: 1.0)
-		navigationBarAppearance.isTranslucent = true
-		navigationBarAppearance.tintColor = UIColor.white
-		navigationBarAppearance.backgroundColor = UIColor.clear
-		navigationBarAppearance.barStyle = .blackTranslucent
-		navigationBarAppearance.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.white]
-		
-		let tableViewAppearance = UITableView.appearance()
-		tableViewAppearance.rowHeight = 55
-		
-		// To be able to get recording information and playback controls in control center.
-		UIApplication.shared.beginReceivingRemoteControlEvents()
-		
-		return true
-	}
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // `persistentContainer` is marked 'lazy', so initialize to ready it for use.
+        _ = CoreDataStack.sharedInstance.viewContext
 
-	func applicationDidEnterBackground(_ application: UIApplication) {
-		// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-		// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-		if !testInitializer.active {
-			CoreDataStack.sharedInstance.saveContext()
-		}
-	}
+        let navigationBarAppearance = UINavigationBar.appearance()
+        navigationBarAppearance.barTintColor = UIColor(red: 231.0 / 255.0, green: 76.0 / 255.0, blue: 60.0 / 255.0, alpha: 1.0)
+        navigationBarAppearance.isTranslucent = true
+        navigationBarAppearance.tintColor = UIColor.white
+        navigationBarAppearance.backgroundColor = UIColor.clear
+        navigationBarAppearance.barStyle = .blackTranslucent
+        navigationBarAppearance.titleTextAttributes = [
+			NSForegroundColorAttributeName: UIColor.white
+		]
 
-	func applicationWillTerminate(_ application: UIApplication) {
-		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-		
-		if !testInitializer.active {
-			CoreDataStack.sharedInstance.saveContext()
-		} else {
-			testInitializer.reset()
-		}
-		
-	}
+        // To be able to get recording information and playback controls in control center.
+        UIApplication.shared.beginReceivingRemoteControlEvents()
+
+        return true
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        CoreDataStack.sharedInstance.saveContext()
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        CoreDataStack.sharedInstance.saveContext()
+    }
 
 }
 
