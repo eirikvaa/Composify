ed622f7102d82eba96d67e10207747abb7703a9e
Eirik Vale Aase <e.v.aase@gmail.com>
2017-12-30 00:58:48 +0100

    Refactoring

--- Composify/SectionCollectionViewDelegate.swift
+++ Composify/SectionCollectionViewDelegate.swift
@@ -11,26 +11,25 @@ class SectionCollectionViewDelegate: NSObject {
 
 // MARK: UICollectionViewDelegate
 extension SectionCollectionViewDelegate: UICollectionViewDelegate {
-	func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {		
-		libraryViewController.currentSection = libraryViewController.currentProject?.sortedSections[indexPath.row]
-		
-		if let recordingViewController = libraryViewController.rootPageViewDataSource.viewController(at: indexPath.row, storyboard: libraryViewController.storyboard!) {
-            
-            if let vc = libraryViewController.rootPageViewController.viewControllers?.first as? RecordingsViewController {
-                if let index = vc.pageIndex {
-                    if index != indexPath.row {
-                        if index > indexPath.row {
-                            libraryViewController.rootPageViewController.setViewControllers([recordingViewController], direction: .reverse, animated: true, completion: nil)
-                        } else {
-                            libraryViewController.rootPageViewController.setViewControllers([recordingViewController], direction: .forward, animated: true, completion: nil)
-                        }
-                    }
-                }
-            }
-			
-		}
+	func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        defer {
+            libraryViewController.setEmptyState()
+        }
         
-		libraryViewController.setEmptyState()
+		libraryViewController.currentSection = libraryViewController.currentProject?.sortedSections[indexPath.row]
+        
+        guard let recordingViewController = libraryViewController
+            .rootPageViewDataSource
+            .viewController(at: indexPath.row, storyboard: libraryViewController.storyboard!) else { return }
+        
+        guard let vc = libraryViewController
+            .rootPageViewController
+            .viewControllers?.first as? RecordingsViewController else { return }
+        
+        guard let pageIndex = vc.pageIndex, pageIndex != indexPath.row else { return }
+        
+        let direction: UIPageViewControllerNavigationDirection = pageIndex > indexPath.row ? .reverse : .forward
+        libraryViewController.rootPageViewController.setViewControllers([recordingViewController], direction: direction, animated: true, completion: nil)
 	}
 }
 
