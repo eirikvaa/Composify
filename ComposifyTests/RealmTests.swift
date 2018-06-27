1a464f57e06373f4194a4dcd0b473d0789148208
Eirik Vale Aase <e.v.aase@gmail.com>
2018-06-27 18:49:06 +0200

    Specify correct iOS version in Podfile

---
+++ ComposifyTests/RealmTests.swift
@@ -0,0 +1,34 @@
+//
+//  RealmTests.swift
+//  ComposifyTests
+//
+//  Created by Eirik Vale Aase on 27.06.2018.
+//  Copyright © 2018 Eirik Vale Aase. All rights reserved.
+//
+
+import XCTest
+import RealmSwift
+
+class RealmTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
