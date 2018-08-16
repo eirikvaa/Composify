76b9b72bc770cf271f2ac41a43b69727fb736fbe
Eirik Vale Aase <e.v.aase@gmail.com>
2018-08-16 08:24:54 +0200

    Move misplaced UITableViewDataSource method

--- Composify/AdministrateProjectTableViewDataSource.swift
+++ Composify/AdministrateProjectTableViewDataSource.swift
@@ -89,4 +89,56 @@ extension AdministrateProjectTableViewDataSource: UITableViewDataSource {
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return administrateProjectViewController.headers[section]
     }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        switch editingStyle {
+        case .insert:
+            guard let currentProject = administrateProjectViewController.currentProject else { return }
+            
+            administrateProjectViewController.insertNewSection {
+                self.administrateProjectViewController.administrateProjectDelegate?.userDidAddSectionToProject($0)
+            }
+            
+            administrateProjectViewController.newValues.removeAll()
+            for (index, sectionID) in currentProject.sectionIDs.enumerated() {
+                // We're skipping the entries that have a title different
+                // from the corresponding section title, because that means the
+                // section was renamed. Without this check, if a section is renamed
+                // and a section later added, the rename will be ignored.
+                guard let section = sectionID.correspondingSection,
+                    let existingTitle = administrateProjectViewController.newValues[HashableTuple((1, index))], existingTitle == section.title else { continue }
+                
+                administrateProjectViewController.newValues[HashableTuple((1, index))] = section.title
+            }
+            
+            // It's important that we reload the previously last row after we
+            // insert the new row so there's no problem with indexes.
+            let newIndexPath = IndexPath(row: currentProject.sectionIDs.count, section: 1)
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        case .delete:
+            guard let currentProject = administrateProjectViewController.currentProject,
+                currentProject.sectionIDs.hasElements else { return }
+            let sectionToDelete = currentProject.sectionIDs[indexPath.row].correspondingSection
+            
+            if UserDefaults.standard.lastSection() == sectionToDelete {
+                UserDefaults.standard.resetLastSection()
+            }
+            
+            administrateProjectViewController.deleteSection(sectionToDelete) {
+                self.administrateProjectViewController.administrateProjectDelegate?.userDidDeleteSectionFromProject()
+            }
+            
+            administrateProjectViewController.newValues.removeAll()
+            for (index, sectionID) in currentProject.sectionIDs.enumerated() {
+                if let section = sectionID.correspondingSection {
+                    administrateProjectViewController.newValues[HashableTuple((1, index))] = section.title
+                }
+            }
+            
+            administrateProjectViewController.tableView?.deleteRows(at: [indexPath], with: .automatic)
+        case .none:
+            break
+        }
+    }
 }
