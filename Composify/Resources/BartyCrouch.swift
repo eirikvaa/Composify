0cb81483297c75de6e081c5d7689ec0029479d9e
Eirik Vale Aase <e.v.aase@gmail.com>
2019-12-07 01:31:06 +0100

    Improve support for BartyCrouch

---
+++ Composify/Resources/BartyCrouch.swift
@@ -0,0 +1,15 @@
+//
+//  BartyCrouch.swift
+//  Composify
+//
+//  Created by Eirik Vale Aase on 07/12/2019.
+//  Copyright © 2019 Eirik Vale Aase. All rights reserved.
+//
+
+import Foundation
+
+enum BartyCrouch {
+    enum SupportedLanguage: String {
+        case norwegian = "nb"
+    }
+}
