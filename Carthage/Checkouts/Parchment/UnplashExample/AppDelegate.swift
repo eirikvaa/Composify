6346f429baa829c57495378a11ab6e10f2c0cefb
Eirik Vale Aase <e.v.aase@gmail.com>
2020-11-14 01:02:51 +0100

    Remove Carthage folder

--- Carthage/Checkouts/Parchment/UnplashExample/AppDelegate.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-// Nothing to see here. Check out ViewController.swift for
-// implementation details.
-
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-  var window: UIWindow?
-}
