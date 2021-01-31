e33e3f7cbf99ba073c9a541e406faf1a1beb9bc6
Eirik Vale Aase <e.v.aase@gmail.com>
2021-01-31 16:10:39 +0100

    Do stuff

--- Composify/UI/HomeView.swift
+++ Composify/UI/HomeView.swift
@@ -7,24 +7,66 @@
 //
 
 import SwiftUI
+import SwiftUIPager
 
 struct HomeView: View {
+    @State private var page = 0
+    private var items = Array(0..<5)
+    private var recordings = Array(0..<5)
+    private var sections = ["Intro", "Verse", "Solo", "Chorus", "Outro"]
+    
     var body: some View {
-        TabView {
-            Text("Studio")
-                .tabItem {
-                    VStack {
-                        Image(systemName: "record.circle")
-                        Text("Record")
+        NavigationView {
+            VStack {
+                Pager(page: $page, data: items, id: \.self) { index in
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text(sections[index])
+                                .font(.title)
+                                .bold()
+                                .padding()
+                            Spacer()
+                        }
+                        ForEach(recordings, id: \.self) { i in
+                            HStack {
+                                Button(action: {
+                                    
+                                }) {
+                                    Image(systemName: "play.circle.fill")
+                                        .resizable()
+                                        .frame(width: 25, height: 25)
+                                        .padding()
+                                        .foregroundColor(.red)
+                                }
+                                .buttonStyle(PlainButtonStyle())
+                                Text("Recording \(i)")
+                                    .padding()
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                            }
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 4.0)
+                                    .stroke()
+                            )
+                        }
+                        .padding(.horizontal)
+                        Spacer()
                     }
                 }
-            Text("Settings")
-                .tabItem {
-                    VStack {
-                        Image(systemName: "gear")
-                        Text("Settings")
-                    }
+                
+                Button(action: {
+                    print("Hei")
+                }) {
+                    Text("Record")
+                        .padding()
+                        .foregroundColor(.white)
                 }
+                .frame(width: 200)
+                .background(Color.red)
+                .cornerRadius(4.0)
+                
+                Spacer()
+            }
+            .navigationBarTitle("Composify", displayMode: .inline)
         }
     }
 }
@@ -32,5 +74,6 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .previewDevice("iPhone 7 Plus")
     }
 }
---
+++ Composify/UI/SectionsPager.swift
@@ -0,0 +1,23 @@
+//
+//  SectionsPager.swift
+//  Composify
+//
+//  Created by Eirik Vale Aase on 14/11/2020.
+//  Copyright © 2020 Eirik Vale Aase. All rights reserved.
+//
+
+import SwiftUI
+
+struct SectionsPager: View {
+    @State private var page = 0
+    
+    var body: some View {
+        Text("Hei")
+    }
+}
+
+struct SectionsPager_Previews: PreviewProvider {
+    static var previews: some View {
+        SectionsPager()
+    }
+}
