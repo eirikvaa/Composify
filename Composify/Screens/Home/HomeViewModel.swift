21e814e35ddb6355caa55ce7ab78e8422d9e13a6
Eirik Vale Aase <e.v.aase@gmail.com>
2021-03-13 01:03:52 +0100

    Add SongRepository and HomeViewModel

---
+++ Composify/Screens/Home/HomeViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewModel.swift
+//  Composify
+//
+//  Created by Eirik Vale Aase on 13/03/2021.
+//  Copyright © 2021 Eirik Vale Aase. All rights reserved.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    
+}
---
+++ Composify/SongRepository.swift
@@ -0,0 +1,29 @@
+//
+//  SongRepository.swift
+//  Composify
+//
+//  Created by Eirik Vale Aase on 13/03/2021.
+//  Copyright © 2021 Eirik Vale Aase. All rights reserved.
+//
+
+import Foundation
+
+protocol SongRepositoryInjectable {
+    var songRepository: SongRepository { get }
+}
+
+extension SongRepositoryInjectable {
+    var songRepository: SongRepository {
+        SongRepositoryImpl()
+    }
+}
+
+protocol SongRepository {
+    func getRecordings(in section: Section) -> [Recording]
+}
+
+class SongRepositoryImpl: SongRepository {
+    func getRecordings(in section: Section) -> [Recording] {
+        []
+    }
+}
