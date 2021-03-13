7fb2ffaddb7966ff5a91227052baa463f32272b0
Eirik Vale Aase <e.v.aase@gmail.com>
2021-03-13 13:56:14 +0100

    Delete old test files

---
+++ ComposifyTests/ComposifyTests.swift
@@ -0,0 +1,18 @@
+//
+//  ComposifyTests.swift
+//  ComposifyTests
+//
+//  Created by Eirik Vale Aase on 13/03/2021.
+//  Copyright © 2021 Eirik Vale Aase. All rights reserved.
+//
+
+import XCTest
+
+class ComposifyTests: XCTestCase {
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+}
