b67e42d175c8d3a396e9acfae14d8772a2add202
Eirik Vale Aase <e.v.aase@gmail.com>
2021-03-14 11:22:32 +0100

    Make HomeView easier to read, improve no projects view state

--- Composify/Screens/Home/HomeView.swift
+++ Composify/Screens/Home/HomeView.swift
@@ -10,76 +10,26 @@ import SwiftUI
 import SwiftUIPager
 
 struct HomeView: View {
-    @StateObject private var viewModel = HomeViewModel()
+    @ObservedObject private var viewModel = HomeViewModel()
     @StateObject private var audioRecorder = AudioRecorder()
     @State private var isShowingNewProjectView = false
     @State private var isShowingEditProjectView = false
     @State private var currentSection = Section()
+
+    init(viewModel: HomeViewModel = HomeViewModel()) {
+        self.viewModel = viewModel
+    }
 
     var body: some View {
         NavigationView {
             VStack {
                 switch (viewModel.currentProject, viewModel.currentSection) {
                 case let (project?, .some):
-                    VStack {
-                        SectionsPager(sections: Array(project.sections), currentSection: $currentSection)
-                        Spacer()
-                        RecordButton(isRecording: $audioRecorder.isRecording) {
-                            if audioRecorder.isRecording {
-                                let url = audioRecorder.stopRecording()
-                                let recording = Recording(
-                                    title: url.lastPathComponent,
-                                    section: currentSection,
-                                    url: url.absoluteString
-                                )
-                                viewModel.save(recording: recording)
-                            } else {
-                                audioRecorder.startRecording()
-                            }
-                        }
-                        .padding(.bottom, 20)
-                    }
-                    .padding()
-                    .toolbar {
-                        ToolbarItem(placement: .bottomBar) {
-                            Button(action: {
-                                isShowingEditProjectView = true
-                            }, label: {
-                                Text("Edit Project")
-                            })
-                        }
-                    }
-                    .sheet(isPresented: $isShowingEditProjectView) {
-                        EditProjectView(project: Binding<Project>(
-                                            get: { project },
-                                            set: { _ in })
-                        ) { _ in
-                            viewModel.loadData()
-                        }
-                    }
+                    loadedView(project: project)
                 case (let project?, nil):
-                    Button(action: {
-                        isShowingEditProjectView = true
-                    }, label: {
-                        Text("Edit \(project.title)")
-                    })
-                        .sheet(isPresented: $isShowingEditProjectView) {
-                            EditProjectView(project: Binding<Project>(
-                                                get: { project },
-                                                set: { _ in })
-                            ) { _ in
-                                viewModel.loadData()
-                            }
-                        }
+                    noSectionsView(project: project)
                 case (nil, nil):
-                    Button(action: {
-                        isShowingNewProjectView = true
-                    }, label: {
-                        Text("Add project")
-                    })
-                    .sheet(isPresented: $isShowingNewProjectView) {
-                        NewProjectView()
-                    }
+                    noProjectsView()
                 case (nil, .some):
                     fatalError("Impossible ot have a section without a project.")
                 }
@@ -90,10 +40,81 @@ struct HomeView: View {
             viewModel.loadData()
         }
     }
+
+    private func noProjectsView() -> some View {
+        Button(action: {
+            isShowingNewProjectView = true
+        }, label: {
+            Text("Add project")
+                .padding()
+                .foregroundColor(.white)
+        })
+        .background(Color.gray)
+        .cornerRadius(10.0)
+        .sheet(isPresented: $isShowingNewProjectView) {
+            NewProjectView()
+        }
+    }
+
+    private func noSectionsView(project: Project) -> some View {
+        Button(action: {
+            isShowingEditProjectView = true
+        }, label: {
+            Text("Edit \(project.title)")
+        })
+            .sheet(isPresented: $isShowingEditProjectView) {
+                EditProjectView(project: Binding<Project>(
+                                    get: { project },
+                                    set: { _ in })
+                ) { _ in
+                    viewModel.loadData()
+                }
+            }
+    }
+
+    private func loadedView(project: Project) -> some View {
+        VStack {
+            SectionsPager(sections: Array(project.sections), currentSection: $currentSection)
+            Spacer()
+            RecordButton(isRecording: $audioRecorder.isRecording) {
+                if audioRecorder.isRecording {
+                    let url = audioRecorder.stopRecording()
+                    let recording = Recording(
+                        title: url.lastPathComponent,
+                        section: currentSection,
+                        url: url.absoluteString
+                    )
+                    viewModel.save(recording: recording)
+                } else {
+                    audioRecorder.startRecording()
+                }
+            }
+            .padding(.bottom, 20)
+        }
+        .padding()
+        .toolbar {
+            ToolbarItem(placement: .bottomBar) {
+                Button(action: {
+                    isShowingEditProjectView = true
+                }, label: {
+                    Text("Edit Project")
+                })
+            }
+        }
+        .sheet(isPresented: $isShowingEditProjectView) {
+            EditProjectView(project: Binding<Project>(
+                                get: { project },
+                                set: { _ in })
+            ) { _ in
+                viewModel.loadData()
+            }
+        }
+    }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        let noProjectsViewModel = HomeViewModel(currentProject: nil, currentSection: nil)
+        HomeView(viewModel: noProjectsViewModel)
     }
 }
